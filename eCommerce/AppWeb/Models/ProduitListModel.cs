24fa53eb01dd7c9cde6ada8b9abf584250a12f66
Guillaume Mariot <guillaume.mariot.gm@gmail.com>
2018-06-09 10:12:32 +0200

    AppWeb

---
+++ eCommerce/AppWeb/Models/ProduitListModel.cs
@@ -0,0 +1,32 @@
+﻿using Modele.Entities.Entity;
+using Service.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWeb.Models
+{
+    public class ProduitListModel
+    {
+        private List<ProduitModels> products;
+
+        public ProduitListModel(List<Products> list)
+        {
+            products = new List<ProduitModels>();
+            foreach (Products prod in list)
+            {
+                products.Add(new ProduitModels(prod));
+            }
+
+        }
+
+        public List<ProduitModels> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+
+    }
+}
---
+++ eCommerce/AppWeb/Models/ProduitModels.cs
@@ -0,0 +1,64 @@
+﻿using Modele.Entities.Entity;
+using Service.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWeb.Models
+{
+    public class ProduitModels
+    {
+        private int _productId;
+        private string _productCode;
+        private string _productLabel;
+        private string _productDescription;
+        private int _productStock;
+        private float _productPrice;
+
+        public ProduitModels(Products p)
+        {
+            this._productId = p.ProductId;
+            this._productCode = p.ProductCode;
+            this._productLabel = p.ProductLabel;
+            this._productDescription = p.ProductDescription;
+            this._productStock = p.ProductStock;
+            this._productPrice = p.ProductPrice;
+        }
+
+        public int ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+
+        public string ProductCode
+        {
+            get { return _productCode; }
+            set {_productCode = value; }
+        }
+        public string ProductLabel
+        {
+            get { return _productLabel; }
+            set {_productLabel = value; }
+        }
+
+        public string ProductDescription
+        {
+            get { return _productCode; }
+            set { _productCode = value; }
+        }
+
+        public int ProductStock
+        {
+            get { return _productStock; }
+            set { _productStock = value; }
+        }
+        public float ProductPrice
+        {
+            get { return _productPrice; }
+            set { _productPrice = value; }
+        }
+
+    }
+}
